c6db63905aa5440123811b13c97558ef6d696776
hazadus <hazadus7@gmail.com>
2025-09-06 13:40:54 +0300

    fix: исправить путь к иконке для системного трея
    - Изменён путь к иконке в `createTray` с `icon_white.png` на `icon.png`

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ function createAboutWindow(): void {
 // Функция создания системного трея
 function createTray(): void {
   // Загружаем иконку
-  const iconPath = path.join(__dirname, '..', 'assets', 'icons', 'icon_white.png');
+  const iconPath = path.join(__dirname, '..', 'assets', 'icons', 'icon.png');
   const icon = nativeImage.createFromPath(iconPath);
   
   // Создаём трей
